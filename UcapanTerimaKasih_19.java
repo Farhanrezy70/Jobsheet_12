186718d010ee914a2f5d307f0b64baa30cfaa39c
Farhanrezy <farhanrezy70@gmail.com>
2023-11-23 08:50:54 +0700

    Percobaan 4

---
+++ UcapanTerimaKasih_19.java
@@ -0,0 +1,33 @@
+package Jobsheet_12;
+import java.util.Scanner;
+
+public class UcapanTerimaKasih_19 {
+
+    public static String PenerimaUcapan() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan:");
+        String namaOrang = sc.nextLine();
+        return namaOrang;
+    }
+
+    public static void UcapanTerimakasih() {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you " + nama + " for being the best teacher in the world.\n" +
+                "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void UcapanTambahan(String tambahan) {
+        System.out.println("Ucapan Tambahan: " + tambahan);
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        UcapanTerimakasih();
+
+        System.out.print("Tuliskan ucapan tambahan: ");
+        UcapanTambahan(sc.nextLine());
+
+        sc.close();
+    }
+}
